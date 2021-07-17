a810c1603ef8badfabba175a617672510ba64005
Jonathan Lester <jonathanmarklester@gmail.com>
2021-07-17 21:47:24 +0100

    add pages

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 document.body.setAttribute('data-theme', `${isDark ? 'dark' : 'light'}`);
 
 const setActive = (day) => {
-  document.querySelector('.date-tab.active').classList.remove("active");
+  document.querySelector('.day-tab.active').classList.remove("active");
   document.querySelector('.planner.active').classList.remove("active");
   document.querySelector(`#${day}-tab`).classList.add("active");
   document.querySelector(`#${day}-planner`).classList.add("active");
+};
+
+const showMenu = () => {
+  if (document.querySelector('#menu').classList.contains('active')) {
+    document.querySelector('#menu').classList.remove('active')
+  } else {
+    document.querySelector('#menu').classList.add('active')
+  }
 };
