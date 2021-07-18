1ffa96ff5e45f112312ec9a0ff7f1bff86a4ef8c
Jonathan Lester <jonathanmarklester@gmail.com>
2021-07-18 19:07:46 +0100

    underline title, set today or closest to active

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const root = document.querySelector(':root');
 const body = document.body;
 const themeButton = document.querySelector('#theme-button');
 
-const colors = {
-  friday: '0, 255, 255',
-  saturday: '255, 0, 255',
-  sunday: '255, 255, 0'
-};
+const days = [
+  {
+    day: 'friday',
+    color: '0, 255, 255',
+    date: '2021-08-13'
+  },
+  {
+    day: 'saturday',
+    color: '255, 0, 255',
+    date: '2021-08-14'
+  },
+  {
+    day: 'sunday',
+    color: '255, 255, 0',
+    date: '2021-08-15'
+  }
+];
 
+const today = new Date().setHours(0, 0, 0, 0);
+const closest = days.reduce((a, b) => Math.abs(new Date(a.date) - today) < Math.abs(new Date(b.date) - today) ? a : b);
 
-root.style.setProperty('--active', colors.friday);
-document.querySelector(`#friday-tab`).classList.add("active");
-document.querySelector(`#friday-whatson`).classList.add("active");
+root.style.setProperty('--active', closest.color);
+document.querySelector(`#${closest.day}-tab`).classList.add("active");
+document.querySelector(`#${closest.day}-whatson`).classList.add("active");
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   body.setAttribute('data-theme', 'dark');
@@ -19,7 +33,7 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 } else {
   body.setAttribute('data-theme', 'light');
   themeButton.textContent = 'dark'
-}
+} 
 
 const changeTheme = () => {
   if (body.getAttribute('data-theme') === 'light') {
@@ -34,7 +48,7 @@ const changeTheme = () => {
 const setActive = (day) => {
   document.querySelector('.day-tab.active').classList.remove("active");
   document.querySelector('.whatson.active').classList.remove("active");
-  root.style.setProperty('--active', colors[day]);
-  document.querySelector(`#${day}-tab`).classList.add("active");
-  document.querySelector(`#${day}-whatson`).classList.add("active");
+  root.style.setProperty('--active', days[day].color);
+  document.querySelector(`#${days[day].day}-tab`).classList.add("active");
+  document.querySelector(`#${days[day].day}-whatson`).classList.add("active");
 };
