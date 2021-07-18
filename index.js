bbaf56846dd360df129aca8b9762c186d614e3a2
Jonathan Lester <jonathanmarklester@gmail.com>
2021-07-18 11:52:39 +0100

    table fixes, add theme button

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-let isDark = false
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  isDark = true;
+  document.body.setAttribute('data-theme', 'dark');
+  document.querySelector('#theme-button').textContent = 'light'
+} else {
+  document.body.setAttribute('data-theme', 'light');
+  document.querySelector('#theme-button').textContent = 'dark'
 }
-document.body.setAttribute('data-theme', `${isDark ? 'dark' : 'light'}`);
+
+const changeTheme = () => {
+  if (document.body.getAttribute('data-theme') === 'light') {
+    document.body.setAttribute('data-theme', 'dark');
+    document.querySelector('#theme-button').textContent = 'light'
+  } else {
+    document.querySelector('#theme-button').textContent = 'dark'
+    document.body.setAttribute('data-theme', 'light')
+  }
+};
 
 const setActive = (day) => {
   document.querySelector('.day-tab.active').classList.remove("active");
-  document.querySelector('.planner.active').classList.remove("active");
+  document.querySelector('.whatson.active').classList.remove("active");
   document.querySelector(`#${day}-tab`).classList.add("active");
-  document.querySelector(`#${day}-planner`).classList.add("active");
-};
-
-const showMenu = () => {
-  if (document.querySelector('#menu').classList.contains('active')) {
-    document.querySelector('#menu').classList.remove('active')
-  } else {
-    document.querySelector('#menu').classList.add('active')
-  }
+  document.querySelector(`#${day}-whatson`).classList.add("active");
 };
