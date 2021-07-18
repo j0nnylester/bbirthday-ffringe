bdeda4d8b8f54454b3b689cd77fd4c70ca2f75b1
Jonathan Lester <jonathanmarklester@gmail.com>
2021-07-18 15:49:38 +0100

    add more event info

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+const root = document.querySelector(':root');
+const body = document.body;
+const themeButton = document.querySelector('#theme-button');
+
+const colors = {
+  friday: '0, 255, 255',
+  saturday: '255, 0, 255',
+  sunday: '255, 255, 0'
+};
+
+
+root.style.setProperty('--active', colors.friday);
+document.querySelector(`#friday-tab`).classList.add("active");
+document.querySelector(`#friday-whatson`).classList.add("active");
+
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.setAttribute('data-theme', 'dark');
-  document.querySelector('#theme-button').textContent = 'light'
+  body.setAttribute('data-theme', 'dark');
+  themeButton.textContent = 'light'
 } else {
-  document.body.setAttribute('data-theme', 'light');
-  document.querySelector('#theme-button').textContent = 'dark'
+  body.setAttribute('data-theme', 'light');
+  themeButton.textContent = 'dark'
 }
 
 const changeTheme = () => {
-  if (document.body.getAttribute('data-theme') === 'light') {
-    document.body.setAttribute('data-theme', 'dark');
-    document.querySelector('#theme-button').textContent = 'light'
+  if (body.getAttribute('data-theme') === 'light') {
+    body.setAttribute('data-theme', 'dark');
+    themeButton.textContent = 'light'
   } else {
-    document.querySelector('#theme-button').textContent = 'dark'
-    document.body.setAttribute('data-theme', 'light')
+    body.setAttribute('data-theme', 'light')
+    themeButton.textContent = 'dark'
   }
 };
 
 const setActive = (day) => {
   document.querySelector('.day-tab.active').classList.remove("active");
   document.querySelector('.whatson.active').classList.remove("active");
+  root.style.setProperty('--active', colors[day]);
   document.querySelector(`#${day}-tab`).classList.add("active");
   document.querySelector(`#${day}-whatson`).classList.add("active");
 };
