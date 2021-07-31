120087eefbd14ccdae71aa7fbcf446218b92a6d1
Jonathan Lester <jonathanmarklester@gmail.com>
2021-07-31 17:47:05 +0100

    update cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'bbff_2021_07'
+const cacheName = 'bbff_2021_08_01'
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(cacheName).then(function (cache) {
@@ -23,6 +23,21 @@ self.addEventListener('fetch', function (event) {
   event.respondWith(
     fetch(event.request).catch(function () {
       return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (name) {
+          return name !== cacheName
+        }).map(function (cacheName) {
+          console.log('Deleting expired cache:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
